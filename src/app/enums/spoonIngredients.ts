e35b2ea4e535b9e81f6813917b98d94154135973
Alex Norton <atn362@gmail.com>
2023-06-06 22:05:24 -0500

    adding logic for the measrument spoon portion

--- src/app/enums/spoonIngredients.ts
+++ src/app/enums/spoonIngredients.ts
@@ -1,4 +1,3 @@
-import { SpoonMeasurements } from './spoonMeasurments';
 export enum SpoonIngredients {
   SALT = 'Salt',
   BAKINGPOWDER = 'Baking Powder',
--- src/app/spoon-measurments/spoon-measurments.component.ts
+++ src/app/spoon-measurments/spoon-measurments.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SpoonIngredients } from '../enums/spoonIngredients';
+import { SpoonMeasurements } from '../enums/spoonMeasurments';
+
 
 @Component({
   selector: 'app-spoon-measurments',
@@ -6,5 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./spoon-measurments.component.css']
 })
 export class SpoonMeasurmentsComponent {
+
+  input!: number;
+  result!: number;
+
+  selectedIngredients!: SpoonIngredients;
+  selectedPortion!: SpoonMeasurements;
+  portions: string[] = Object.values(SpoonMeasurements);
+  ingredients: string[] = Object.values(SpoonIngredients);
+
+  handleIngredientSelection(): void {
+    switch(this.selectedIngredients){
+      case SpoonIngredients.BAKINGPOWDER:
+        this.result = Math.floor(this.input * 5);
+        break;
+      case SpoonIngredients.BAKINGSODA:
+        this.result = Math.floor(this.input * 6);
+        break;
+      case SpoonIngredients.CINNAMON:
+        this. result = Math.floor(this.input * 3);
+        break;
+        default:
+          console.log('Unknown Selection');
+          break;
+    }
+  }
+
+  hanglePortionSize(): void {
+    switch(this.selectedPortion) {
+      case SpoonMeasurements.EIGHTTSP:
+        this.input = .125;
+        break;
+      case SpoonMeasurements.QUARTERTSP:
+        this.input = .25;
+        break;
+      case SpoonMeasurements.HALFTSP:
+        this.input = .5;
+        break;
+      case SpoonMeasurements.THREEQUARTERTSP:
+        this.input = .75;
+        break;
+      case SpoonMeasurements.TEASPOON:
+        this.input = 1;
+        break;
+      case SpoonMeasurements.QUARTERTBSP:
+        this.input = .75;
+        break;
+      case SpoonMeasurements.HALFTBSP:
+        this.input = 1.5;
+        break;
+      case SpoonMeasurements.THREEQUARTERTBSP:
+        this.input = 2.25;
+        break;
+      case SpoonMeasurements.TABLESPOON:
+        this.input = 3;
+        break;
+        default:
+        console.log('Unknown Selection');
+        break;
+    }
+  }
+
+
 
 }
